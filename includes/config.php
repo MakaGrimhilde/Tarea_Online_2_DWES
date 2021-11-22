c81dadfb51726f68cfd15f7c07c9e11eaa3ca2e1
MakaGrimhilde <simtoclatro@gmail.com>
2021-11-22 21:01:48 +0100

    Modificación de config.php

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,5 @@
 <?php
 
- 
  $dbHost = 'localhost';
  $dbName = 'bdusuarios';
  $dbUser = 'root';
@@ -12,11 +11,11 @@
     $conexion = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo '<div class="alert alert-success row justify-content-center">'."Conectado a la Base de Datos de usuarios!! :)".'</div>';
+    echo '<div class="mx-auto col-sm-4 alert alert-success row justify-content-center">'."Conectado a la Base de Datos de usuarios :)".'</div>';
 
  } catch (PDOException$ex){
 
-    echo'<div class="alert alert-danger row justify-content-center">'."No se pudo conectar a la BD de usuarios!! :( <br/>"
+    echo'<div class="mx-auto col-sm-4 alert alert-danger row justify-content-center">'."No se pudo conectar a la BD de usuarios :( <br/>"
     .$ex->getMessage().'</div>'; 
 
  }
